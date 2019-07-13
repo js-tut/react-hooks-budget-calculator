627be1b82d99edf08d6e3550b9d96b86f9137ba2
js-tut <johnsmilga@gmail.com>
2019-07-13 09:14:25 -0700

    12-controlled-inputs

--- src/App.js
+++ src/App.js
@@ -10,14 +10,39 @@ const initialExpenses = [
   { id: uuid(), charge: "credit card bill", amount: 1200 }
 ];
 function App() {
+  // ************* state values *******************
+  // all expenses, add expense
   const [expenses, setExpenses] = useState(initialExpenses);
+  // single expense
+  const [charge, setCharge] = useState("");
+  // single amount
+  const [amount, setAmount] = useState("");
+  // ************* functionality *******************
+  const handleCharge = e => {
+    console.log(`charge :  ${e.target.value}`);
 
+    setCharge(e.target.value);
+  };
+  const handleAmount = e => {
+    console.log(`amount :  ${e.target.value}`);
+
+    setAmount(e.target.value);
+  };
+  const handleSubmit = e => {
+    e.preventDefault();
+  };
   return (
     <>
       <Alert />
       <h1>budget calculator</h1>
       <main className="App">
-        <ExpenseForm />
+        <ExpenseForm
+          charge={charge}
+          amount={amount}
+          handleAmount={handleAmount}
+          handleCharge={handleCharge}
+          handleSubmit={handleSubmit}
+        />
         <ExpenseList expenses={expenses} />
       </main>
       <h1>
