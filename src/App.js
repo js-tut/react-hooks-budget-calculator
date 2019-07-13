9be026c1dd335b94f693b47d4cdce81729821438
js-tut <johnsmilga@gmail.com>
2019-07-13 09:46:32 -0700

    13-handle-submit

--- src/App.js
+++ src/App.js
@@ -19,17 +19,21 @@ function App() {
   const [amount, setAmount] = useState("");
   // ************* functionality *******************
   const handleCharge = e => {
-    console.log(`charge :  ${e.target.value}`);
-
     setCharge(e.target.value);
   };
   const handleAmount = e => {
-    console.log(`amount :  ${e.target.value}`);
-
     setAmount(e.target.value);
   };
   const handleSubmit = e => {
     e.preventDefault();
+    if (charge !== "" && amount > 0) {
+      const singleExpense = { id: uuid(), charge, amount };
+      setExpenses([...expenses, singleExpense]);
+      setCharge("");
+      setAmount("");
+    } else {
+      // handle alert called
+    }
   };
   return (
     <>
@@ -50,7 +54,7 @@ function App() {
         <span className="total">
           $
           {expenses.reduce((acc, curr) => {
-            return (acc += curr.amount);
+            return (acc += parseInt(curr.amount));
           }, 0)}
         </span>
       </h1>
